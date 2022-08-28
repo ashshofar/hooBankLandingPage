4f8b98788c7aa2443f557bfa6c87d511fe429f8c
inu <inu@voxteneo.com>
2022-08-28 16:17:41 +0700

    init commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam illum
+        sequi expedita nobis vitae labore voluptatibus delectus, est blanditiis
+        praesentium commodi reiciendis ullam dolor? Voluptate veritatis voluptas
+        dolores modi dicta!
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
